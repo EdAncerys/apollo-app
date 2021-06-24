c744fa5bcb88bb1e51c98a21e2efabf802caa2ac
Ed <lookatemail@gmail.com>
2021-06-24 20:01:15 +0100

    Add login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
+import React, { useState, useEffect, useMemo } from 'react';
+
 import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
+
+export const AppContext = React.createContext();
 
 function App() {
+  const [accessToken, setAccessToken] = useState(false);
+  const [page, setPage] = useState(false);
+  const [alert, setAlert] = useState(false);
+  const [spinner, setSpinner] = useState(false);
+
+  const SESSION_STORAGE_KEY = 'oneTouchPortal.App';
+
+  const manageAppContext = useMemo(
+    () => ({
+      accessToken,
+      setAccessToken,
+      page,
+      setPage,
+      alert,
+      setAlert,
+      spinner,
+      setSpinner,
+    }),
+    [accessToken, page, alert, spinner]
+  );
+
+  useEffect(() => {
+    const data = sessionStorage.getItem(SESSION_STORAGE_KEY);
+    const userSession = JSON.parse(data);
+    if (userSession) setAccessToken(userSession.accessToken);
+    if (userSession) setPage(userSession.page);
+  }, []); // eslint-disable-line
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>Hello World!</div>
-      </header>
-    </div>
+    <AppContext.Provider
+      value={{
+        manageAppContext,
+      }}
+    >
+      <div className="app">
+        <div className="container">
+          <div>Hello World!</div>
+          <Login />
+        </div>
+      </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+export default function login({ props }) {
+  return (
+    <div className="features">
+      <div className="flex-container-50">
+        <Form className="form-container">
+          <Form.Group className="mb-3">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              id="loginEmail"
+              type="email"
+              placeholder="Enter email"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              id="loginPassword"
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Button
+            // onClick={() => userLogin()}
+            variant="primary"
+            size="lg"
+            className="shadow-none"
+          >
+            Log In
+          </Button>
+          <div className="divider"></div>
+          <Button
+            // onClick={() => manageAppContext.setPage('create-new-account')}
+            variant="success"
+            size="lg"
+            className="shadow-none"
+          >
+            Create New Account
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
+}
